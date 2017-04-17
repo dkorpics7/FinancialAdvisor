f7c7e39f13ec4635a9cf73898175dbacebcd108f
dkorpics7 <dkorpics@roadrunner.com>
2017-04-17 12:29:58 -0400

    Added main menu and client information update menu

--- FinancialAdvisor/Client.cs
+++ FinancialAdvisor/Client.cs
@@ -101,33 +101,33 @@ namespace FinancialAdvisor
 
             string input;                   //user input string
             bool valid = false;             //indicates whether user input is a valid entry
-            bool quit = true;
+            bool exit = true;
             string[] prompt = { };
 
-            quit = GetClientAddress(clientNumber, newclient);
-            if (quit) quit = GetClientCity(clientNumber, newclient);
-            if (quit) quit = GetClientState(clientNumber, newclient);
-            if (quit) quit = GetClientZip(clientNumber, newclient);
-            if (quit) quit = GetClientPhone(clientNumber, newclient);
-            if (quit) quit = GetClientCell(clientNumber, newclient);
+            exit = GetClientAddress(clientNumber, newclient);
+            if (exit) exit = GetClientCity(clientNumber, newclient);
+            if (exit) exit = GetClientState(clientNumber, newclient);
+            if (exit) exit = GetClientZip(clientNumber, newclient);
+            if (exit) exit = GetClientPhone(clientNumber, newclient);
+            if (exit) exit = GetClientCell(clientNumber, newclient);
 
-            return quit;
+            return exit;
         }
 
         public bool GetClientAddress(int clientNumber, Client newclient)
         {
-            bool quit = true;                //indicates whether user wants to quit the program
+            bool exit = true;                //indicates whether user wants to exit the program
             bool valid = false;              //indicates whether user input is a valid entry
             string input;                    //user input string
 
-            while (quit && !valid)          //get street address
+            while (exit && !valid)          //get street address
             {
                 Console.WriteLine("\r\n\tEnter street adress or P.O Box (e.g. 123 Main St.");
                 Console.Write("\r\n\tAddress:             ");
                 input = Console.ReadLine().Trim();
-                if (input.ToLower() == "quit")
+                if (input.ToLower() == "exit")
                 {
-                    quit = false;
+                    exit = false;
                 }
                 else
                 {
@@ -136,22 +136,22 @@ namespace FinancialAdvisor
                 }
             }
 
-            return quit;
+            return exit;
         }
 
         public bool GetClientCity(int clientNumber, Client newclient)
         {
-            bool quit = true;                //indicates whether user wants to quit the program
+            bool exit = true;                //indicates whether user wants to exit the program
             bool valid = false;              //indicates whether user input is a valid entry
             string input;                    //user input string
 
-            while (quit && !valid)          //get city
+            while (exit && !valid)          //get city
             {
                 Console.Write("\r\n\tEnter city:          ");
                 input = Console.ReadLine().Trim();
-                if (input.ToLower() == "quit")
+                if (input.ToLower() == "exit")
                 {
-                    quit = false;
+                    exit = false;
                 }
                 else
                 {
@@ -160,22 +160,22 @@ namespace FinancialAdvisor
                 }
             }
 
-            return quit;
+            return exit;
         }
 
         public bool GetClientState(int clientNumber, Client newclient)
         {
-            bool quit = true;                //indicates whether user wants to quit the program
+            bool exit = true;                //indicates whether user wants to exit the program
             bool valid = false;              //indicates whether user input is a valid entry
             string input;                    //user input string
 
-            while (quit && !valid)          //get state
+            while (exit && !valid)          //get state
             {
                 Console.Write("\r\n\tEnter state:         ");
                 input = Console.ReadLine().Trim();
-                if (input.ToLower() == "quit")
+                if (input.ToLower() == "exit")
                 {
-                    quit = false;
+                    exit = false;
                 }
                 else
                 {
@@ -184,22 +184,22 @@ namespace FinancialAdvisor
                 }
             }
 
-            return quit;
+            return exit;
         }
 
         public bool GetClientZip(int clientNumber, Client newclient)
         {
-            bool quit = true;                //indicates whether user wants to quit the program
+            bool exit = true;                //indicates whether user wants to exit the program
             bool valid = false;              //indicates whether user input is a valid entry
             string input;                    //user input string
 
-            while (quit && !valid)          //get zip code
+            while (exit && !valid)          //get zip code
             {
                 Console.Write("\r\n\tEnter zip code:      ");
                 input = Console.ReadLine().Trim();
-                if (input.ToLower() == "quit")
+                if (input.ToLower() == "exit")
                 {
-                    quit = false;
+                    exit = false;
                 }
                 else
                 {
@@ -208,22 +208,22 @@ namespace FinancialAdvisor
                 }
             }
 
-            return quit;
+            return exit;
         }
 
         public bool GetClientPhone(int clientNumber, Client newclient)
         {
-            bool quit = true;                //indicates whether user wants to quit the program
+            bool exit = true;                //indicates whether user wants to exit the program
             bool valid = false;              //indicates whether user input is a valid entry
             string input;                    //user input string
 
-            while (quit && !valid)          //get home phone
+            while (exit && !valid)          //get home phone
             {
                 Console.Write("\r\n\tEnter home phone:    ");
                 input = Console.ReadLine().Trim();
-                if (input.ToLower() == "quit")
+                if (input.ToLower() == "exit")
                 {
-                    quit = false;
+                    exit = false;
                 }
                 else
                 {
@@ -232,22 +232,22 @@ namespace FinancialAdvisor
                 }
             }
 
-            return quit;
+            return exit;
         }
 
         public bool GetClientCell(int clientNumber, Client newclient)
         {
-            bool quit = true;                //indicates whether user wants to quit the program
+            bool exit = true;                //indicates whether user wants to exit the program
             bool valid = false;              //indicates whether user input is a valid entry
             string input;                    //user input string
 
-            while (quit && !valid)          //get cell phone number
+            while (exit && !valid)          //get cell phone number
             {
                 Console.Write("\r\n\tEnter cell phone:    ");
                 input = Console.ReadLine().Trim();
-                if (input.ToLower() == "quit")
+                if (input.ToLower() == "exit")
                 {
-                    quit = false;
+                    exit = false;
                 }
                 else
                 {
@@ -256,9 +256,122 @@ namespace FinancialAdvisor
                 }
             }
 
-            return quit;
+            return exit;
         }
 
+        public bool ViewClientInfo(int clientNumber, Client client)
+        {
+            //This method displays Client information and allows them to update their information
+
+            int menuOption;                  //user menu choice
+            bool exit = true;                //indicates whether user wants to exit the program
+            bool valid = false;              //indicates whether user input is a valid entry
+            string input;                    //user input string
+
+            while (exit && !valid)
+            {
+                //Display Client Information
+                Console.Clear();
+                Console.WriteLine("\r\n\t\t\tProfile for {0} {1}", client.FName, client.LName);
+                Console.WriteLine("\t\t==================================================\r\n\r\n");
+                Console.WriteLine("\t\tAddress:          " + client.Address1);
+                Console.WriteLine("\t\t                  {0}, {1}  {2}", client.City, client.State, client.Zip);
+                Console.WriteLine("\r\n\t\tHome Phone:       " + client.HomePhone);
+                Console.WriteLine("\t\tCell Phone:       " + client.CellPhone + "\r\n");
+
+                //Update Client Information
+                Console.Write("\r\n\tWould you like to update your information (y or n)?  ");
+                input = Console.ReadLine().Trim().ToLower();
+
+                if (input == "y" || input == "yes")                   //Display menu for updating info
+                {
+                    Console.Clear();
+                    Console.WriteLine("\r\n\t\t\tUPDATE MENU");
+                    Console.WriteLine("\t\t     ================\r\n\r\n");
+                    Console.WriteLine("\t1.  Update street address");
+                    Console.WriteLine("\t2.  Update city");
+                    Console.WriteLine("\t3.  Update state");
+                    Console.WriteLine("\t4.  Update zip code");
+                    Console.WriteLine("\t5.  Update home phone");
+                    Console.WriteLine("\t6.  Update cell phone");
+                    Console.WriteLine("\t7.  Exit");
+
+                    Console.Write("\r\n\r\n\tEnter menu choice:  ");  
+                    input = Console.ReadLine().Trim().ToLower();      //Get user menu choice
+                    menuOption = GetInput(input);
+                    if (menuOption == -2) menuOption = 7;
+
+                    switch (menuOption)
+                    {
+                        case 1:
+                            exit = GetClientAddress(clientNumber, client);
+                            break;
+                        case 2:
+                            exit = GetClientCity(clientNumber, client);
+                            break;
+                        case 3:
+                            exit = GetClientState(clientNumber, client);
+                            break;
+                        case 4:
+                            exit = GetClientZip(clientNumber, client);
+                            break;
+                        case 5:
+                            exit = GetClientPhone(clientNumber, client);
+                            break;
+                        case 6:
+                            exit = GetClientCell(clientNumber, client);
+                            break;
+                        case 7:
+                            exit = false;
+                            break;
+                        default:
+                            Console.WriteLine("\r\n*** Invalid Entry.");
+                            break;
+                    }
+
+                }
+                else if (input.Substring(0, 1) == "e")
+                {
+                    exit = false;
+                }
+                else
+                {
+                    valid = true;
+                }
+            }
+            return exit;
+        }
+
+        static int GetInput(string input) // parses input string to return a valid user-selected menu option
+        {
+            int selectedOption;
+            double decimalNumber;
+            bool validInt = false;
+            bool validDouble = false;
+
+            validInt = int.TryParse(input, out selectedOption);
+            validDouble = double.TryParse(input, out decimalNumber);
+
+            if (validInt)
+            {
+                return selectedOption;                       // a valid integer was entered
+            }
+            else if (validDouble)                             // a decimal number was entered
+            {
+                selectedOption = Convert.ToInt32(decimalNumber);  // convert it to an integer
+                return selectedOption;
+            }
+            else if (input == "exit" || input == "quit" || input == "e")
+            {
+                selectedOption = -2;                           // user wants to exit program
+                return selectedOption;
+            }
+            else
+            {
+                selectedOption = -1;
+                return selectedOption;
+            }
+        }
 
     }
 }
--- FinancialAdvisor/Program.cs
+++ FinancialAdvisor/Program.cs
@@ -13,21 +13,21 @@ namespace FinancialAdvisor
             //This program keeps track of clients and their account balances
             //Clients can have checking and/or savings accounts
 
-            int menuOption;                 //user's menu option entered
+            int menuOption = 0;             //user's menu option entered
             int lastChecking = 0;           //last checking account number assigned
             int lastSavings = 0;            //last savings account number assigned
             int clientNumber = -1;          //client identification number (used internally)
-            string input;                   //user input
+            string input = "";              //user input
             string fName = "";              //user first name
             string lName = "";              //user last name
             string timeOfDay;               //morning, afternoon, or evening
-            bool quit = true;               //if quit==false, user wants to end program
+            bool exit = true;               //if exit==false, user wants to end program
             bool valid = false;             //tests user input to see if it is a valid 
 
 
             //instantiate some existing clients into a list
             List<Client> clientList = new List<Client>();
-            clientList.Add(new Client("Diane","Korpics",1,"941 Westhaven Drive","","Hudson","Ohio","44236","(330) 687-2486","(330) 687-2486)"));
+            clientList.Add(new Client("Diane","Korpics",1,"941 Westhaven Drive","","Hudson","Ohio","44236","(330) 687-2486","(330) 687-2486"));
             clientList.Add(new Client("Daniel", "Vivacqua", 2));
             clientList.Add(new Client("Jarryd", "Hurtley", 3));
             clientList.Add(new Client("Dave", "Kolar", 4));
@@ -48,14 +48,14 @@ namespace FinancialAdvisor
             Console.WriteLine("\r\n\r\n\t\t!!!!! Welcome to Financial Solutions !!!!!");
             Console.WriteLine("\r\n\t\t    We help make your dreams come true.");
  
-            //Ask them to "login".  Store their first and last name.
-            while (quit && !valid)
+            //Ask user to "login".  Store their first and last name.
+            while (exit && !valid)
             {
                 Console.Write("\r\n\r\n\r\n\tPlease login (e.g., John Smith):  ");
                 input = Console.ReadLine().Trim();
                 fName = GetFirstName(input);
                 lName = GetLastName(input);
-                if (input.ToLower() == "quit") quit = false;     //determine if user wants to quit
+                if (input.ToLower() == "exit") exit = false;     //determine if user wants to exit
                 if (fName == "" || lName == "")                  //user must enter BOTH a first and last name
                 {
                     Console.Clear();
@@ -66,13 +66,12 @@ namespace FinancialAdvisor
                     Console.Write("\r\n\r\n\tYou entered {0} {1}.  Is this Correct (y or n)? ", fName, lName);
                     input = Console.ReadLine().Trim();
                     if (input.Substring(0, 1).ToLower() == "y") valid = true;
-                    if (input.Substring(0, 1).ToLower() == "q") quit = false;
+                    if (input.Substring(0, 1).ToLower() == "e") exit = false;
                 }
             }
 
-            while (quit)
+            if (exit)
             {
-
                 //Determine if the user is an existing client  (retrieve or assign client number)
                 foreach (Client c in clientList)
                 {
@@ -85,11 +84,10 @@ namespace FinancialAdvisor
                     Console.Clear();
                     Console.WriteLine("\r\n\t\t{0}, Welcome to Financial Solutions!",fName);
                     Console.WriteLine("\r\n\r\n\tPlease follow the prompts below to set up your account(s).");
-                    Console.WriteLine("\tNOTE: You may enter \"quit\" at any time to leave our site.\r\n");
+                    Console.WriteLine("\tNOTE: You may enter \"exit\" at any time to leave our site.\r\n");
                     clientNumber = clientList.Count + 1;
                     clientList.Add(new Client(fName, lName, clientNumber));
-                    quit = clientList[clientList.Count-1].EnterClientInfo(clientNumber, clientList[clientNumber-1]);
-                    Console.ReadKey();
+                    exit = clientList[clientList.Count-1].EnterClientInfo(clientNumber, clientList[clientNumber-1]);
                 }
                 else
                 //If an existing client, greet client
@@ -97,31 +95,68 @@ namespace FinancialAdvisor
                     Console.Clear();
                     Console.WriteLine("\r\n\t\tWelcome back {0} and good {1}!", fName, timeOfDay);
                 }
-                foreach (Client c in clientList)
-                {
-                    Console.WriteLine("\r\n" + c.FName);
-                    Console.WriteLine(c.Address1);
-                    Console.WriteLine(c.City);
-                    Console.WriteLine(c.State);
-                    Console.WriteLine(c.Zip);
-                    Console.WriteLine(c.HomePhone);
-                    Console.WriteLine(c.CellPhone);
+            }
 
+            while(exit && menuOption !=5)
+            {
+                //Main Menu -- keeps returning here until user asks to exit program
+                DisplayMainMenu();
+
+                //Get menu option from user
+                Console.Write("\r\n\r\n\tEnter menu option:  ");
+                input = Console.ReadLine().Trim().ToLower();
+
+                menuOption = GetInput(input);
+                if (menuOption == -2) menuOption = 5;
+                switch (menuOption)
+                {
+                    case 1:                         //View Client Info
+                        exit = clientList[clientNumber-1].ViewClientInfo(clientNumber, clientList[clientNumber - 1]);
+                        break;
+                    case 2:                         //View Account Balance(s)
+                        break;
+                    case 3:                         //Make a Deposit
+                        break;
+                    case 4:                         //Make a Withdrawal
+                        break;
+                    case 5:                         //Exit
+                        exit = false;
+                        break;
+                    default:                        //Invalid input
+                        Console.WriteLine("\r\n*** Invalid Input Option.\r\n");
+                        break;
                 }
 
+                //foreach (Client c in clientList)
+                //{
+                //    Console.WriteLine("\r\n" + c.FName);
+                //    Console.WriteLine(c.Address1);
+                //    Console.WriteLine(c.City);
+                //    Console.WriteLine(c.State);
+                //    Console.WriteLine(c.Zip);
+                //    Console.WriteLine(c.HomePhone);
+                //    Console.WriteLine(c.CellPhone);
+                //}
+
                 Console.ReadKey();
-                quit = false;
+                exit = false;
 
-            }
-            //Get menu option from user
 
-            //view account balances
+                //view account balances
 
-            //make a deposit
+                //make a deposit
 
-            //make a withdrawal
+                //make a withdrawal
 
-            Console.WriteLine("\r\n\r\nPress any key to continue ...");
+
+
+            } // end Main Menu loop
+
+            //Thank user for their business and exit program
+            Console.Clear();
+            Console.WriteLine("\r\n\r\n\t!!!!!   Thank you for your business   !!!!!");
+            Console.WriteLine("\r\nWe hope your experience was a pleasant one. Have a nice {0}.", timeOfDay);
+            Console.WriteLine("\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\nPress any key to continue ...");
             Console.ReadKey();
         }
 
@@ -133,7 +168,7 @@ namespace FinancialAdvisor
             {
                 timeOfDay = "morning";
             }
-            else if(DateTime.Now.Hour >= 12 && DateTime.Now.Hour < 5)
+            else if(DateTime.Now.Hour >= 12 && DateTime.Now.Hour < 17)
             {
                 timeOfDay = "afternoon";
             }
@@ -141,6 +176,7 @@ namespace FinancialAdvisor
             {
                 timeOfDay = "evening";
             }
+
             return timeOfDay;
         }
 
@@ -174,8 +210,20 @@ namespace FinancialAdvisor
             {
                 lastName = input.Substring(input.LastIndexOf(" ")+1,(input.Length-input.LastIndexOf(" ")-1));
             }
-
+            
             return lastName;
+        }
+
+        static void DisplayMainMenu()  //displays main menu
+        {
+            Console.Clear();
+            Console.WriteLine("\r\n\r\n\t\t\tMAIN MENU");
+            Console.WriteLine("\t\t    ================\r\n\r\n");
+            Console.WriteLine("\t1.  View/Update your information.");
+            Console.WriteLine("\t2.  View Account Balance.");
+            Console.WriteLine("\t3.  Deposit Funds.");
+            Console.WriteLine("\t4.  Withdraw Funds.");
+            Console.WriteLine("\t5.  Exit.");
         }
 
         static int GetInput(string input) // parses input string to return a valid user-selected menu option
@@ -194,12 +242,12 @@ namespace FinancialAdvisor
             }
             else if (validDouble)                             // a decimal number was entered
             {
-                selectedOption = Convert.ToInt32(decimalNumber);  // convert to an integer
+                selectedOption = Convert.ToInt32(decimalNumber);  // convert it to an integer
                 return selectedOption;
             }
-            else if (input == "quit" || input == "exit" || input == "q")
+            else if (input == "exit" || input == "quit" || input == "e")
             {
-                selectedOption = 6;
+                selectedOption = -2;                           // user wants to exit program
                 return selectedOption;
             }
             else
@@ -207,20 +255,6 @@ namespace FinancialAdvisor
                 selectedOption = -1;
                 return selectedOption;
             }
-        }
-        static string GetPetName(string userName)  //Asks the user for the pet name
-        {
-            string petName;
-
-            Console.Write("\r\n\r\n\r\n\t{0}, What would you like to call your pet?\r\n\t(if you hit return, we will pick a name for you):  ", userName);
-            petName = Console.ReadLine().Trim();
-
-            if (petName == "") Console.WriteLine("\r\n\r\n\tYour pet's name is George, and he is lucky to have such a \r\n\tconscientious owner.", petName);
-            Console.WriteLine("\r\n\r\n\r\nPress any key to continue...");
-            Console.ReadKey();
-            Console.Clear();
-
-            return petName;
         }
     }
 }
